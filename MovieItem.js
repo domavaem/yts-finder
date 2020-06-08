9f3ac472c51c9fd05353e53a5c0c0bd8122ac435
domavaem <domavaem@gmail.com>
2020-06-09 05:20:05 +0900

    Update: MovieItem

--- MovieItem.js
+++ MovieItem.js
@@ -2,10 +2,23 @@ import React from "react";
 
 const MovieItem = ({ data, onClickEvent }) => {
   //console.log("data", data);
+  let genres = "";
+  data.genres.forEach((v, i) => {
+    if (i != 0) genres += ", ";
+    genres += v;
+  });
   return (
     <div className="movie" onClick={onClickEvent} data={JSON.stringify(data)}>
-      <div>{`Title: ${data.title}`}</div>
-      <div>{`Rating: ${data.rating}`}</div>
+      <div className="movie-data-1">
+        <img src={`${data.medium_cover_image}`}></img>
+        <h2>{`${data.rating}`}</h2>
+      </div>
+      <div className="movie-data-2">
+        <h2>{`${data.title}`}</h2>
+        <h4>{`${data.year}`}</h4>
+        <h6>{genres}</h6>
+        <p>{`${data.summary.slice(0, 180)}`}...</p>
+      </div>
     </div>
   );
 };
